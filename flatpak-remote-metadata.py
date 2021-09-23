ab90666eef063c5f9ce09ef7984b44e2b8c70bde
gasinvein <gasinvein@gmail.com>
2021-09-23 23:26:34 +0300

    Create temporary flatpak installation

--- flatpak-remote-metadata.py
+++ flatpak-remote-metadata.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import json
 import sys
 import re
@@ -8,8 +9,10 @@ import typing as t
 import gi
 
 gi.require_version("GLib", "2.0")
+gi.require_version("Gio", "2.0")
 gi.require_version("Flatpak", "1.0")
 from gi.repository import GLib
+from gi.repository import Gio
 from gi.repository import Flatpak
 
 
@@ -58,7 +61,7 @@ def metadata_to_dict(metadata: GLib.KeyFile):
     return result
 
 
-def get_apps_metadata(installation: Flatpak.Installation, remote: str = "flathub"):
+def get_apps_metadata(installation: Flatpak.Installation, remote: str):
     apps = []
     for ref in installation.list_remote_refs_sync_full(remote, Flatpak.QueryFlags.NONE):
         if ref.get_kind() != Flatpak.RefKind.APP:
@@ -74,8 +77,28 @@ def get_apps_metadata(installation: Flatpak.Installation, remote: str = "flathub
 
 
 def main():
-    inst = Flatpak.Installation.new_user()
-    metas = [metadata_to_dict(m) for m in get_apps_metadata(inst)]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--url")
+    parser.add_argument("repo_name")
+    args = parser.parse_args()
+
+    cache_home = Gio.File.new_for_path(GLib.get_user_cache_dir())
+    inst_dir = cache_home.get_child("flatpak-remote-metadata").get_child("inst")
+    if not inst_dir.query_exists():
+        inst_dir.make_directory_with_parents()
+    inst = Flatpak.Installation.new_for_path(inst_dir, True)
+
+    try:
+        remote = inst.get_remote_by_name(args.repo_name)
+    except GLib.Error as err:
+        if args.url and err.matches(Flatpak.error_quark(), Flatpak.Error.REMOTE_NOT_FOUND):
+            remote = Flatpak.Remote.new(args.repo_name)
+            remote.set_url(args.url)
+            inst.add_remote(remote, if_needed=True)
+        else:
+            raise
+
+    metas = [metadata_to_dict(m) for m in get_apps_metadata(inst, remote.get_name())]
     json.dump(metas, sys.stdout, indent=4)
 
 
